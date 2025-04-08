770f30063accec81317d032a59fad85e07e646cc
Edward Minaya <edmiya.dev@gmail.com>
2025-04-07 21:12:10 -0400

    feat: initialize project with TypeScript, Express, and Vercel configuration
    - Added package.json with dependencies for Express, CORS, and Resend
    - Created tsconfig.json for TypeScript configuration
    - Added Vercel configuration file for API routing

---
+++ index.js
@@ -0,0 +1,40 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const resend_1 = require("resend");
+const app = (0, express_1.default)();
+const cors = require("cors");
+app.use(cors());
+app.use(express_1.default.json());
+const resend = new resend_1.Resend("re_E4wFpiXY_VkictX5j5T4nwtQmDRVQ9qWq");
+app.post("/send-email", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { name, phone, email, message } = req.body;
+    const { data, error } = yield resend.emails.send({
+        from: "Acme <onboarding@resend.dev>",
+        to: ["edmiya.dev@gmail.com"],
+        subject: "hello world",
+        html: "<strong>Hola mi amor, este es un correo para ti </strong>",
+    });
+    if (error) {
+        res.status(400).json({ error });
+    }
+    else {
+        res.status(200).json({ data });
+    }
+    console.log(req);
+}));
+app.listen(3002, () => {
+    console.log("Listening on http://localhost:3002");
+});
---
+++ index.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from "express";
+import { Resend } from "resend";
+
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const resend = new Resend("re_E4wFpiXY_VkictX5j5T4nwtQmDRVQ9qWq");
+
+app.post("/send-email", async (req, res) => {
+  const { name, phone, email, message } = req.body;
+
+  const { data, error } = await resend.emails.send({
+    from: "Acme <onboarding@resend.dev>",
+    to: ["edmiya.dev@gmail.com"],
+    subject: "Mensaje de contacto desde la web",
+    html: `
+        <strong>Hola, tienes un nuevo mensaje de contacto:</strong><br>
+        <strong>Nombre:</strong> ${name}<br>
+        <strong>Teléfono:</strong> ${phone}<br>
+        <strong>Email:</strong> ${email}<br>
+        <strong>Mensaje:</strong> ${message}
+    `,
+  });
+
+  if (error) {
+    res.status(400).json({error});
+  } else {
+    res.status(200).json({ data });
+  }
+
+  console.log(req);
+});
+
+app.listen(3002, () => {
+  console.log("Listening on http://localhost:3002");
+});
