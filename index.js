991d02316230341e3771325fc084d4ffe7a883d9
Edward Minaya <edmiya.dev@gmail.com>
2025-04-07 22:18:57 -0400

    refactor: comment out email sending functionality in API

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -27,28 +18,27 @@ app.get("/", (req, res) => {
     });
 });
 const resend = new resend_1.Resend("re_E4wFpiXY_VkictX5j5T4nwtQmDRVQ9qWq");
-app.post("/send-email", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { name, phone, email, message } = req.body;
-    const { data, error } = yield resend.emails.send({
-        from: "Acme <onboarding@resend.dev>",
-        to: ["edmiya.dev@gmail.com"],
-        subject: "Mensaje de contacto desde la web",
-        html: `
-        <strong>Hola, tienes un nuevo mensaje de contacto:</strong><br>
-        <strong>Nombre:</strong> ${name}<br>
-        <strong>Teléfono:</strong> ${phone}<br>
-        <strong>Email:</strong> ${email}<br>
-        <strong>Mensaje:</strong> ${message}
-    `,
-    });
-    if (error) {
-        res.status(400).json({ error });
-    }
-    else {
-        res.status(200).json({ data });
-    }
-    console.log(req);
-}));
+// app.post("/send-email", async (req, res) => {
+//   const { name, phone, email, message } = req.body;
+//   const { data, error } = await resend.emails.send({
+//     from: "Acme <onboarding@resend.dev>",
+//     to: ["edmiya.dev@gmail.com"],
+//     subject: "Mensaje de contacto desde la web",
+//     html: `
+//         <strong>Hola, tienes un nuevo mensaje de contacto:</strong><br>
+//         <strong>Nombre:</strong> ${name}<br>
+//         <strong>Teléfono:</strong> ${phone}<br>
+//         <strong>Email:</strong> ${email}<br>
+//         <strong>Mensaje:</strong> ${message}
+//     `,
+//   });
+//   if (error) {
+//     res.status(400).json({error});
+//   } else {
+//     res.status(200).json({ data });
+//   }
+//   console.log(req);
+// });
 app.listen(3000, () => {
     console.log("Listening on http://localhost:3000");
 });
--- index.ts
+++ index.ts
@@ -18,30 +18,30 @@ app.get("/", (req: Request, res: Response) => {
 
 const resend = new Resend("re_E4wFpiXY_VkictX5j5T4nwtQmDRVQ9qWq");
 
-app.post("/send-email", async (req, res) => {
-  const { name, phone, email, message } = req.body;
+// app.post("/send-email", async (req, res) => {
+//   const { name, phone, email, message } = req.body;
 
-  const { data, error } = await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
-    to: ["edmiya.dev@gmail.com"],
-    subject: "Mensaje de contacto desde la web",
-    html: `
-        <strong>Hola, tienes un nuevo mensaje de contacto:</strong><br>
-        <strong>Nombre:</strong> ${name}<br>
-        <strong>Teléfono:</strong> ${phone}<br>
-        <strong>Email:</strong> ${email}<br>
-        <strong>Mensaje:</strong> ${message}
-    `,
-  });
+//   const { data, error } = await resend.emails.send({
+//     from: "Acme <onboarding@resend.dev>",
+//     to: ["edmiya.dev@gmail.com"],
+//     subject: "Mensaje de contacto desde la web",
+//     html: `
+//         <strong>Hola, tienes un nuevo mensaje de contacto:</strong><br>
+//         <strong>Nombre:</strong> ${name}<br>
+//         <strong>Teléfono:</strong> ${phone}<br>
+//         <strong>Email:</strong> ${email}<br>
+//         <strong>Mensaje:</strong> ${message}
+//     `,
+//   });
 
-  if (error) {
-    res.status(400).json({error});
-  } else {
-    res.status(200).json({ data });
-  }
+//   if (error) {
+//     res.status(400).json({error});
+//   } else {
+//     res.status(200).json({ data });
+//   }
 
-  console.log(req);
-});
+//   console.log(req);
+// });
 
 app.listen(3000, () => {
   console.log("Listening on http://localhost:3000");
